83079ee7b222728622807b638e2770e5ec37e791
HarshEkbote <HarVibhu10@gmail.com>
2025-08-10 11:20:34 +0530

    Colpali comments added

--- backend/core/colpali_client.py
+++ backend/core/colpali_client.py
@@ -1,18 +1,22 @@
 import torch
-from PIL import Image
 from typing import List
 from colpali_engine.models import ColPali,ColPaliProcessor
 
 class ColpaliClient:
     def __init__(self,model_name:str='vidore/colpali-v1.3',cache_dir:str="./model_cache"):
+        
+        #Define the available device
         device='cuda' if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
         self.device=device
         
+        #Declare the colpali model
         self.model=ColPali.from_pretrained(
             pretrained_model_name_or_path=model_name,
             torch_dtype=torch.bfloat16,
             device_map=self.device,
             cache_dir=cache_dir,
+            #Use if using only CPU is used
+            low_cpu_mem_usage=True #remove this if using GPU
         )
         
         self.processor=ColPaliProcessor.from_pretrained(
@@ -21,6 +25,9 @@ class ColpaliClient:
         )
     
     def get_image_embeddings(self,images:List)->List[List[float]]:
+        '''
+        Creates embeddings for the given image or list of images using Colpali
+        '''
         with torch.no_grad():
             image_inputs=self.processor.process_images(images).to(self.model.device)
             image_embeddings=self.model(**image_inputs)
@@ -28,6 +35,9 @@ class ColpaliClient:
         return embeddings
         
     def get_query_embeddings(self,query:str)->List:
+        '''
+        Creates embeddings for the given text query or list of queries using Colpali
+        '''
         with torch.no_grad():
             text_embeddings=self.processor.process_queries([query]).to(self.model.device)
             text_embeddings=self.model(**text_embeddings)
